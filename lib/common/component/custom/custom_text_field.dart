91acfc8c5f997f6cfe2dbfe6639fa503ec7d1daf
Seungmoky <xkwksdnjs@gmail.com>
2024-09-15 18:00:34 +0900

    로그인 - 컴포넌트 분리 및 정리 완료 (연결만 남음)

--- lib/common/component/custom/custom_text_field.dart
+++ lib/common/component/custom/custom_text_field.dart
@@ -1,44 +1,78 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final TextEditingController? controller;
   final TextInputAction? textInputAction;
   final bool obscureText;
   final String labelText;
+
+  /// (value) { _handle(); } 형식
+  final Function(String)? onSubmitted;
 
   const CustomTextField({
     this.controller,
     this.textInputAction,
     this.obscureText = false,
     this.labelText = 'labelText',
+    this.onSubmitted,
     super.key
   });
+
+  /// ErrorText를 외부에서 계속 불러와야하므로 public으로 선언
+  @override
+  CustomTextFieldState createState() => CustomTextFieldState();
+}
+
+class CustomTextFieldState extends State<CustomTextField> {
+  String? errorText;
+
+  /// 오류 메시지가 있다면 String, 없다면 null을 입력 (null일때 입력 필요)
+  void setError(String? error) {
+    setState(() {
+      errorText = error; // 오류 메시지 상태를 변경
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller, // 입력값을 컨트롤러로 받음
-      textInputAction: textInputAction,
-      obscureText: obscureText,
+      controller: widget.controller,
+      textInputAction: widget.textInputAction,
+      onSubmitted: widget.onSubmitted,
+      obscureText: widget.obscureText,
+      cursorColor: Color(0xFF5589D3),
       decoration: InputDecoration(
+        labelText: widget.labelText,
+        labelStyle: TextStyle(
+          color: Color(0xFF5589D3),
+        ),
+        // 일반 상태 테두리
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide(
             color: Color(0xFF5589D3),
           ),
         ),
+        // 포커스된 상태의 테두리
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(
             color: Color(0xFF5589D3),
           ),
         ),
-        labelText: labelText,
-        labelStyle: TextStyle(
-          color: Color(0xFF5589D3),
+        // 오류 메시지
+        errorText: errorText,
+        // 오류가 있을 때 테두리
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.red,
+          ),
+        ),
+        // 포커스된 오류 상태의 테두리 색상
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.red,
+          ),
         ),
       ),
-      cursorColor: Color(0xFF5589D3),
     );
   }
 }
--- lib/screen/user/login_screen.dart
+++ lib/screen/user/login_screen.dart
@@ -5,6 +5,7 @@ import 'package:laxy/common/component/background.dart';
 import 'package:laxy/common/component/custom/custom_app_bar.dart';
 import 'package:laxy/common/component/custom/custom_text_field.dart';
 import 'package:laxy/common/component/page_route_with_animation.dart';
+import 'package:laxy/common/component/show_dialog.dart';
 import 'package:laxy/screen/user/register_screen.dart';
 import 'package:laxy/common/const/constants.dart';
 import 'package:http/http.dart' as http;
@@ -19,19 +20,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<CustomTextFieldState> emailKey = GlobalKey<CustomTextFieldState>();
+  final GlobalKey<CustomTextFieldState> passwordKey = GlobalKey<CustomTextFieldState>();
 
-  // 간단한 이메일 및 비밀번호 검증 함수
+  // 이메일 및 비밀번호 검증
   bool _validateForm() {
     String email = emailController.text;
     String password = passwordController.text;
-    if (email.isEmpty || password.isEmpty) {
-      _showErrorDialog('이메일과 비밀번호를 입력하세요.');
+    if (email.isEmpty && password.isEmpty){
+      emailKey.currentState?.setError('이메일을 입력하세요.');
+      passwordKey.currentState?.setError('비밀번호를 입력하세요.');
+      return false;
+    }
+    if (email.isEmpty){
+      emailKey.currentState?.setError('이메일을 입력하세요.');
+      return false;
+    }
+    if (password.isEmpty) {
+      passwordKey.currentState?.setError('비밀번호를 입력하세요.');
       return false;
     }
     if (!_isEmailValid(email)) {
-      _showErrorDialog('유효한 이메일을 입력하세요.');
+      emailKey.currentState?.setError('유효한 이메일 형식이 아닙니다.');
       return false;
     }
+    emailKey.currentState?.setError(null);
+    passwordKey.currentState?.setError(null);
     return true;
   }
 
@@ -43,27 +57,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return regExp.hasMatch(email);
   }
 
-  // 오류 메시지 다이얼로그
-  void _showErrorDialog(String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('오류'),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('확인'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+  // TODO: 연결 필요
   // 로그인 처리 함수 (API 요청)
   Future<void> _handleLogin() async {
     if (_validateForm()) {
@@ -93,18 +87,25 @@ class _LoginScreenState extends State<LoginScreen> {
           Navigator.pushReplacementNamed(context, '/home');
         } else {
           // 로그인 실패 시 처리 (오류 메시지 보여주기)
-          _showErrorDialog('로그인에 실패했습니다. 상태 코드: ${response.statusCode}');
+          showErrorDialog(context, '로그인에 실패했습니다. 상태 코드: ${response.statusCode}');
         }
       } on TimeoutException catch (_) {
         // 시간 초과 오류 처리
-        _showErrorDialog('요청이 시간 초과되었습니다. 인터넷 연결을 확인해 주세요.');
+        showErrorDialog(context, '요청이 시간 초과되었습니다. 인터넷 연결을 확인해 주세요.');
       } catch (error) {
         // 네트워크 오류 처리
-        _showErrorDialog('로그인 요청 중 오류가 발생했습니다: $error');
+        showErrorDialog(context, '로그인 요청 중 오류가 발생했습니다: $error');
       } finally {
         client.close(); // HTTP 클라이언트 자원 해제
       }
     }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 
   @override
@@ -154,6 +155,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Column(
                         children: [
                           CustomTextField(
+                            key: emailKey,
                             controller: emailController, // 입력값을 컨트롤러로 받음
                             textInputAction: TextInputAction.next,
                             labelText: '이메일',
@@ -161,9 +163,11 @@ class _LoginScreenState extends State<LoginScreen> {
                           SizedBox(height: 20),
                           // 비밀번호 입력 필드
                           CustomTextField(
+                            key: passwordKey,
                             controller: passwordController, // 입력값을 컨트롤러로 받음
                             obscureText: true,
                             labelText: '비밀번호',
+                            onSubmitted: (value) {_handleLogin();},
                           ),
                         ],
                       ),
